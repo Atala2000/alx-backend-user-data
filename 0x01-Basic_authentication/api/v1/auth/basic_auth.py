2dbd73af45cf554c5a36efdc37905e0bff8b78ca
Atala2000 <atalakidi@gmail.com>
2024-04-16 18:53:51 +0300

    Added user search

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -83,3 +83,17 @@ class BasicAuth(Auth):
             if users[0].is_valid_password(user_pwd):
                 return users[0]
         return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves user instance of a request
+        """
+        auth_header = self.authorization_header(request)
+        extracted_header = self.extract_base64_authorization_header(
+            auth_header
+            )
+        decoded_header = self.decode_base64_authorization_header(
+            extracted_header
+            )
+        email, password = self.extract_user_credentials(decoded_header)
+        return self.user_object_from_credentials(email, password)
