00a76ae9e3a79f711d1383d38e0c8bd40f87fb01
Atala2000 <atalakidi@gmail.com>
2024-04-16 09:22:56 +0300

    Correction to type casting

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -29,13 +29,13 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> dict:
+def unauthorized(error) -> str:
     """Unauthorized handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def unallowed(error) -> dict:
+def unallowed(error) -> str:
     """Error handler for authenticated users but unauthorized"""
     return jsonify({"error": "Forbidden"}), 403
 
