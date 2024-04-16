14102fcf410cf53aed9c3238df2f4fbe4121481d
Atala2000 <atalakidi@gmail.com>
2024-04-16 18:17:57 +0300

    Added user search

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,10 @@
 Module that creates a BasicAuth Class
 """
 import base64
+from models.user import User
 import logging
 from api.v1.auth.auth import Auth
+from typing import TypeVar
 
 logging.basicConfig(filename='app.log', filemode='a')
 
@@ -64,3 +66,35 @@ class BasicAuth(Auth):
             return (None, None)
         credentials = decoded_base64_authorization_header.partition(':')
         return (credentials[0], credentials[-1])
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """Returns a user instance based on email and password
+        """
+        # Check if user_email or user_pwd is not a string or None
+        if not isinstance(user_email, str) or user_email is None:
+            return None
+        if not isinstance(user_pwd, str) or user_pwd is None:
+            return None
+
+        # Search for user in the database
+        users = User.search({"email": user_email})
+
+        # If user not found, return None
+        if not users:
+            return None
+
+        # If multiple users found, log a warning and return None
+        if len(users) > 1:
+            print("Warning: Multiple users found with the same email.")
+            return None
+
+        user = users[0]  # Get the first user from the list
+
+        # Check if provided password is valid
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        # Return the user instance if authentication is successful
+        return user
