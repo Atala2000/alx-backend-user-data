9fe353f1b556fd831e734f9b9c4536b7c225a06e
Atala2000 <atalakidi@gmail.com>
2024-04-17 23:08:03 +0300

    Changed switch statements to if statements cause of python version

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -16,14 +16,16 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
+auth = None
 auth_type = os.getenv("AUTH_TYPE")
-match auth_type:
-    case 'auth':
-        auth = Auth()
-    case 'basic_auth':
-        auth = BasicAuth()
-    case 'session_auth':
-        auth = SessionAuth()
+
+if auth_type == 'auth':
+    auth = Auth()
+elif auth_type == 'basic_auth':
+    auth = BasicAuth()
+elif auth_type == 'session_auth':
+    auth = SessionAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
