bf5dc5abf20b3c1a20108ff4ab204bdb3e9917fc
Atala2000 <atalakidi@gmail.com>
2024-04-18 12:03:19 +0300

    Added method to check cookie

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -51,10 +51,13 @@ def filter_user() -> None:
         return
     if not auth.require_auth(
         request.path, ["/api/v1/status/",
-                       "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+                       "/api/v1/unauthorized/", "/api/v1/forbidden/",
+                       "/api/v1/auth_session/login/"
+                       ]
     ):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None and \
+            auth.session_cookie(request) is None:
         abort(401)
     if not auth.current_user(request):
         abort(403)
